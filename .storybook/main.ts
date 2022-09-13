a354a04125ce8b15133977754d1c6f5a645419bd
Tim Phillips <tim@deep6.ai>
2022-09-13 16:04:58 -0700

    convert storybook config to typescript
    https://storybook.js.org/docs/react/configure/overview#configure-your-project-with-typescript

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/core-common";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: (config) => {
+    const path = require("path");
+    // add monorepo root as a valid directory to import modules from
+    config.resolve.plugins.forEach((p) => {
+      if (Array.isArray(p.appSrcs)) {
+        p.appSrcs.push(path.join(__dirname, "..", "..", "..", "storybook"));
+      }
+    });
+    return config;
+  },
+};
+
+module.exports = config;
